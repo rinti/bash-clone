54fef21458fbe428026ef2f89ca4a2d1112f147e
rinti <andreas@agoodid.se>
2016-10-17 22:39:30 +0200

    add hot reloading css

--- base/settings.py
+++ base/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'loremipsum')
 DEBUG = True
 
 ALLOWED_HOSTS = []
+INTERNAL_IPS = ['127.0.0.1']
 
 
 # Application definition
@@ -121,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "base/static"), ]
